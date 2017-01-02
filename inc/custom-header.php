9e9098a9c992e099477819969bcc4e6a82657acf
kingRayhan <rayhan095@gmail.com>
2017-01-02 19:51:51 +0600

    add a default image in custom header

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -18,7 +18,7 @@
  */
 function cleanblog_custom_header_setup() {
 	add_theme_support( 'custom-header', apply_filters( 'cleanblog_custom_header_args', array(
-		'default-image'          => '',
+		'default-image'          => get_template_directory_uri().'/img/home-bg.jpg',
 		'default-text-color'     => '000000',
 		'width'                  => 1000,
 		'height'                 => 250,
